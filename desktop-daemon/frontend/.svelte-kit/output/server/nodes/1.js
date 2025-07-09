a1dcd129084f4d8f76b014db17f011d9197b164e
Will Gilpin <wgilpin@gmail.com>
2025-07-09 13:54:20 +0100

    Properly exclude SvelteKit build files from git tracking
    - Updated .gitignore with correct patterns for .svelte-kit/ and build/ directories
    - Removed all previously tracked build files from git index
    - Build files will now be properly ignored on future builds

--- desktop-daemon/frontend/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CqLKpqBL.js","_app/immutable/chunks/CBJthVVo.js","_app/immutable/chunks/IHki7fMi.js","_app/immutable/chunks/B2wwTmVY.js","_app/immutable/chunks/YhXVieLi.js"];
-export const stylesheets = [];
-export const fonts = [];
