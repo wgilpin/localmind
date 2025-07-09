bedbc6762714d146cbf2b5280dc5ae2b77ef02c7
Will Gilpin <wgilpin@gmail.com>
2025-07-09 23:32:55 +0100

    feat: Include URL when saving pages from Chrome extension

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,11 +1,13 @@
 (() => {
   const pageTitle = document.title;
+  const pageUrl = window.location.href;
   const pageContent = document.body.innerText.substring(0, 500); // Simplified content
 
   chrome.runtime.sendMessage({
     action: 'pageDetails',
     data: {
       title: pageTitle,
+      url: pageUrl,
       content: pageContent
     }
   });
