0aa5546a3b2626e88c6a6382a5d66e72eb25e9e1
Will Gilpin <wgilpin@gmail.com>
2025-07-10 00:26:15 +0100

    fix(chrome-extension): Remove content truncation

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,12 +1,12 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'sendPageData') {
-    const { title, content } = message.data;
+  if (message.action === 'pageDetails') {
+    const { title, content, url } = message.data;
     fetch('http://localhost:3000/documents', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ title, content })
+      body: JSON.stringify({ title, content, url })
     })
     .then(response => response.json())
     .then(data => {
--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,7 +1,7 @@
 (() => {
   const pageTitle = document.title;
   const pageUrl = window.location.href;
-  const pageContent = document.body.innerText.substring(0, 500); // Simplified content
+  const pageContent = document.body.innerText; // Simplified content
 
   chrome.runtime.sendMessage({
     action: 'pageDetails',
